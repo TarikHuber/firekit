32bfbbf5ae83b17eecc97958c1516d720bb289ad
Tarik Huber <huber.tarik@gmail.com>
2018-03-05 13:59:52 +0100

    Updated dependencies and fixed bug in path selector

--- src/store/paths/selectors.js
+++ src/store/paths/selectors.js
@@ -1,12 +1,12 @@
 
-export function getAllPaths(state) {
+export function getAllPaths (state) {
   return state.paths
 }
 
-export function getPath(state, location) {
+export function getPath (state, location) {
   if (state.paths !== undefined && state.paths[location]) {
     return state.paths[location]
   }
 
-  return null
+  return {}
 }
