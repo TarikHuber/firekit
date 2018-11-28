8fa608dea7a8bb1ba30a7506f4f3e50acc26f1b5
Tarik Huber <huber.tarik@gmail.com>
2018-11-28 09:01:11 +0100

    Cleaned up dependencies

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,24 +1,10 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import { Provider } from 'react-redux';
-import FirebaseProvider from '../firekit-provider/FirebaseProvider';
-import configureStore from './store';
-import { Root } from './containers/Root';
-import { addLocalizationData } from './locales';
-import { firebaseApp } from './firebase';
-
-const store = configureStore();
-
-addLocalizationData();
 
 class Demo extends Component {
   render() {
     return <div>
-      <Provider store={store}>
-        <FirebaseProvider firebaseApp={firebaseApp}>
-          <Root />
-        </FirebaseProvider>
-      </Provider>
+      Demo is in React Most Wanted Project
     </div>
   }
 }
